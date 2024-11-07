6455f3d8cbfe660c697149bd05fc9cffc2490d92
DBENTLEY <david.bentley95@gmail.com>
2024-11-07 06:50:07 -0800

    Add computer option functions and array
    Add array of rock paper and scissor for computer
    to choose from.
    
    Add function to generate a random int from 0-2 in order
    to index off the array.
    
    Add function to update computer choice based on the random
    int index of the array.

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ let playerChoice = "";
 let computerChoice = "";
 let rockButton = document.getElementById("rock");
 
+//initialize computer options array
+const computerOptions = ["Rock", "Paper", "Scissors"]
+
+//game functions
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function updateComputerChoice() {
+    computerChoice = computerOptions[getRandomInt(3)];
+    console.log("Computer choice is: " + computerChoice);
+}
+
 //event handler function 
 function updatePlayerChoice(event) {
     playerChoice = event.target.textContent;
-    console.log(playerChoice);
+    console.log("Player choice is: " + playerChoice);
 }
 
 //add event listeners 
