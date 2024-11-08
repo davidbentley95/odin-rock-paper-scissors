a317353dd8ec3f142df27ab1425030d70e75c216
DBENTLEY <david.bentley95@gmail.com>
2024-11-08 06:38:14 -0800

    Add game logic
    Add functions to check for win status and run game logic
    on button click (control by event listener).
    
    Make html text change based on results and choices.
    
    BUG: the choices and results do not reset and just keep
    appending text. Need to clear previous text and replace with new.

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+//next is to add game logic and control flow (maybe add function that calls update computer choice then runs the checks?)
+//maybe the above function runs onClick of the button?
+//function to check if player won?
+
 //initialize variables
 let playerChoice = "";
 let computerChoice = "";
+let playerCounter = 0;
+let computerCounter = 0;
+let playerChoiceText = document.querySelector(".player-choice");
+let computerChoiceText = document.querySelector(".computer-choice");
+let resultsText = document.querySelector(".results");
+
+//event handler function 
+function updatePlayerChoice(event) {
+    playerChoice = event.target.textContent;
+    console.log("Player choice is: " + playerChoice);
+    updateComputerChoice();
+    playingGame();
+}
+
+//add event listeners 
+document.getElementById("rock").addEventListener("click", updatePlayerChoice);
+document.getElementById("paper").addEventListener("click", updatePlayerChoice);
+document.getElementById("scissors").addEventListener("click", updatePlayerChoice);
 
 //initialize computer options array
 const computerOptions = ["Rock", "Paper", "Scissors"]
@@ -15,18 +37,27 @@ function updateComputerChoice() {
     console.log("Computer choice is: " + computerChoice);
 }
 
-//event handler function 
-function updatePlayerChoice(event) {
-    playerChoice = event.target.textContent;
-    console.log("Player choice is: " + playerChoice);
-    updateComputerChoice();
+function playingGame() {
+
+    playerChoiceText.textContent += playerChoice;
+    computerChoiceText.textContent += computerChoice;
+    
+    if(playerChoice === computerChoice) {
+        resultsText.textContent += "Draw, try again!"
+    } else if(playerWins()) {
+        resultsText.textContent += "You Win!!";
+        playerCounter++
+    } else {
+        resultsText.textContent += "You LOSE!!";
+        computerCounter++
+    }
+
 }
 
-//add event listeners 
-document.getElementById("rock").addEventListener("click", updatePlayerChoice);
-document.getElementById("paper").addEventListener("click", updatePlayerChoice);
-document.getElementById("scissors").addEventListener("click", updatePlayerChoice);
+function playerWins() {
+    
+    if((playerChoice === "Rock" && computerChoice === "Scissors") || (playerChoice === "Paper" && computerChoice === "Rock") || (playerChoice === "Scissors" && computerChoice === "Paper")) {
+        return true;
+    }
 
-//next is to add game logic and control flow (maybe add function that calls update computer choice then runs the checks?)
-//maybe the above function runs onClick of the button?
-//function to check if player won?
+}
